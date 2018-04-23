9ebdd46227d14565aed4a0648b5d7546fdad7234
nessfullstackteama <34310296+nessfullstackteama@users.noreply.github.com>
2018-04-23 20:44:15 +0200

    REST API improvements

--- backend/user/UserController.js
+++ backend/user/UserController.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var { isNullOrUndefined } = require('util');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
@@ -25,9 +26,41 @@ router.post('/', function (req, res) {
 });
 
 // RETURNS ALL THE USERS IN THE DATABASE
-router.get('/', function (req, res) {
+router.get('/', function (req, res) { 
+    var sql = 'SELECT `id`, `personalNumber`, `email`, `status` FROM `User`';
+
+    if (!isNullOrUndefined(req.query.sort_by)) {
+        var sortCols = req.query.sort_by.split(',');
+        var whereArray = [];
+        var regex = /^(asc|desc)\((\w+)\)$/i;
+
+        for (i = 0; i < sortCols.length; i++) {
+            var match = regex.exec(sortCols[i]);
+
+            if (match.length == 3) {
+                whereArray.push(match[2].toLowerCase() + ' ' + match[1].toUpperCase());
+            }
+        }
+
+        if (whereArray.length > 0) {
+            sql += 'WHERE ' + whereArray.join(', ');
+        }
+        
+/*
+        if (['id', 'personalNumber', 'email', 'status'].includes(req.query.sort_by)) {
+
+        }*/
+    }
+
+    req.query.limit = req.query.limit || 100;
+    sql += ' LIMIT ' + req.query.limit;
+
+    if (!isNullOrUndefined(req.query.offset)) {
+        sql += ' OFFSET ' + req.query.offset;
+    }
+
     db.query(
-        'SELECT `id`, `personalNumber`, `email`, `status` FROM `User`', 
+        sql, 
         function(err, results, fields) {
             if (err) return res.status(500).send("There was a problem finding the users:\n" + err);
             res.status(200).send(results);
